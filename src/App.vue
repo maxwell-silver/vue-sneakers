5fb12ec0976d40dec18bfa1d4c7866f13635459d
Ilya Rodin <i1.rodin@yandex.ru>
2023-12-17 20:12:59 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import HelloBlock from './components/HelloBlock.vue'
+import { v4 as uuid } from 'uuid'
+import { reactive } from 'vue'
+
+interface IGood {
+  title: string
+  price: number
+  isAdded: boolean
+  id: string
+}
+
+const goods = reactive<IGood[]>([
+  { title: 'Кроссовки Puma', price: 9500, isAdded: false, id: uuid() },
+  { title: 'Кроссовки Nike', price: 15000, isAdded: true, id: uuid() },
+  { title: 'Кроссовки New Balance', price: 20000, isAdded: true, id: uuid() }
+])
+</script>
+
+<template>
+  <div v-for="good in goods" :key="good.id" class="m-4">
+    <HelloBlock
+      :title="good.title"
+      :price="good.price"
+      :is-added="good.isAdded"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloBlock.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  price: number
+  isAdded: boolean
+}>()
+</script>
+
+<template>
+  <div class="border-2 rounded mb-2 p-4 text-slate-700">
+    <p>
+      Товар: <span class="font-bold">{{ title }}</span>
+    </p>
+    <p>
+      Цена: <span class="font-bold">{{ price }}</span>
+    </p>
+    <p>Добавлен: {{ isAdded }}</p>
+  </div>
+</template>
